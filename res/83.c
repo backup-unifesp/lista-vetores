ce0385d020395a6287d84eacb606cc2473a569fc
backup-unifesp <vinicius.coral@unifesp.br>
2026-04-23 16:59:02 -0300

    Increase array sizes from 10 to 20 in 83.c

--- res/83.c
+++ res/83.c
@@ -3,24 +3,24 @@
 #include <stdio.h>
 
 int main() {
-    float num[10] = {};
-    float inv[10] = {};
+    float num[20] = {};
+    float inv[20] = {};
     int i, i2 = 0;
     
-    for(i=0; i<10; i++) {
+    for(i=0; i<20; i++) {
         printf("\nDigite o %d elemento: ", i+1);
         scanf("%f", &num[i]);
     }
-    for(i=9; i>=0; i--) {
+    for(i=19; i>=0; i--) {
         inv[i] = num[i2];
         i2++;
     }
     printf("\n\nNúmeros fornecidos: ");
-    for(i=0; i<10; i++) {
+    for(i=0; i<20; i++) {
         printf(" %.2f |", num[i]);
     }
     printf("\n\nOrdem inversa: ");
-    for(i=0; i<10; i++) {
+    for(i=0; i<20; i++) {
         printf(" %.2f |", inv[i]);
     }
     return 0;
